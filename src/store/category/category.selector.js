e36f264bfae6357fada8678bd085c965d893f06a
lilsizzy <vienbaoduc6@gmail.com>
2022-09-16 21:33:52 +0700

    fix loi

--- src/store/category/category.selector.js
+++ src/store/category/category.selector.js
@@ -9,11 +9,11 @@ export const selectCategories = createSelector(
 
 export const selectCategoriesMap = createSelector(
   [selectCategories],
-  (categories) => {
-    return categories.reduce((acc, category) => {
+  (categories) => 
+    categories.reduce((acc, category) => {
       const { title, items } = category;
       acc[title.toLowerCase()] = items;
       return acc;
     })
-  }
+  
 );
